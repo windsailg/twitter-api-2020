99621f83fd18cadc39521e7f9a971a1c4a7c787b
REN <windsailg@gmail.com>
2020-12-28 12:13:25 +0800

    fix cors

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(cors())
 app.use(flash())
 app.use(methodOverride('_method'))
 app.engine('hbs', hbs({
